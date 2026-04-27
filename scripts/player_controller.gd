6e2bca21af4cf79c8a4da532bbe0930b99364b90
kornelmrozcs <kornelmrozcs@gmail.com>
2026-04-27 15:46:46 +0100

    Add first player movement prototype and test scene

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,25 +1,57 @@
 extends CharacterBody2D
+class_name PlayerController
+
+@export var move_speed = 300.0
+@export var jump_velocity = -400.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction = 0.0
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+func _physics_process(delta):
+	# gravity
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# jump (basic)
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	# movement left/right
+	direction = Input.get_axis("move_left", "move_right")
+
+	if direction != 0:
+		velocity.x = direction * move_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, move_speed)
 
 	move_and_slide()
+
+	# TODO / PLAN (state machine)
+
+# na razie jest jeden script z ifami, ale później to przerobić na state machine
+
+# planowane stany:
+# - idle (stoi na ziemi, brak inputu)
+# - run (ruch po ziemi)
+# - jump (idzie do góry)
+# - fall (spada)
+# - wall slide (ślizga się po ścianie)
+# - respawn (brak kontroli, reset)
+
+# pomysł:
+# każdy state osobny plik
+# np. idle_state.gd, run_state.gd itd.
+
+# player_controller będzie tylko:
+# - trzymał velocity, gravity itd.
+# - zmieniał aktualny state
+
+# później do tego podpiąć animacje zamiast robić if velocity.y < 0 itd.
+
+# dodatkowe rzeczy do dodania później:
+# - coyote time
+# - jump buffer
+# - wall jump
+# - hazards / respawn
+# - collectables
